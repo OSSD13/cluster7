cfd92fafa855b8dee41d0650412c92bd429f8a37
Natchaya <chocolatesunm@gmail.com>
2025-04-08 22:14:54 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -228,7 +228,7 @@
                         </span>
                     </a>
                     @endif
-                    @if(auth()->user()->isAdmin() || auth()->user()->isTester())
+                    @if(auth()->user()->isAdmin())
                     <div class="px-4 py-2 mt-6 mb-6" x-show="sidebarOpen" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
                         <p class="text-xs uppercase font-semibold text-gray-500 mt-1 mb-1 tracking-wider">Admin Settings</p>
                     </div>
